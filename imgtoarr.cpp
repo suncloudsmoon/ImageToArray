368885c4111f9a67192d38a5088a2e01b0c8d9fe
suncloudsmoon <gajjam2003@gmail.com>
2022-05-14 11:04:24 -0700

    Fixed bugs

--- imgtoarr.cpp
+++ imgtoarr.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2022, suncloudsmoon and the ImageToArray contributors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "imgtoarr.hpp"
 
 namespace util {
@@ -13,7 +32,7 @@ namespace util {
 	}
 
 	std::string arrtosource(const ImageBin& b) {
-		std::string out = "char img[" + std::to_string(b.size()) + "] = {";
+		std::string out = "unsigned char img[" + std::to_string(b.size()) + "] = {";
 		if (b.empty()) return (out += "\n};\n");
 		
 		std::size_t i;
@@ -22,8 +41,7 @@ namespace util {
 			out += std::to_string(b.at(i)) + ",";
 		}
 		if (i != 0) out += std::to_string(b.at(i));
-		out += "\n};\n";
 		
-		return out;
+		return (out += "\n};\n");
 	}
 }
